358b54e2927ad3da863f39802991f92f54216f36
kabdulka <kenabdulka@gmail.com>
2023-12-02 17:14:34 -0800

    Clean up header.scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,40 +1,40 @@
-
 import "./Header.scss";
 
 // import logo from '../../assets/Logo/BrainFlix-logo.svg';
 import logo from "../../assets/Logo/BrainFlix-logo.svg";
-import avatar from '../../assets/Images/Mohan-muruge.jpg';
+import avatar from "../../assets/Images/Mohan-muruge.jpg";
 import { Link } from "react-router-dom";
 
-
 const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="header__logo-container">
+          <Link className="header__logo-link" to="/">
+            <img
+              className="header__logo-image"
+              src={logo}
+              alt="brainflix-logo"
+            />
+          </Link>
+        </div>
 
-    return ( 
-        <>
-            <header className="header">
-                <div className="header__logo-container">
-                    
-                    <Link className='header__logo-link' to="/" >
-                        <img className="header__logo-image" src={logo} alt="brainflix-logo"/> 
-                    </Link>
-
-                </div>
-
-                <div className="header__content">
-                    <input className="header__search-input" type="text" id="search" name="search" placeholder="Search"/>
-                    <img src={avatar} className="header__avatar" />
-                    <Link className="header__upload" to="/upload">
-                        UPLOAD
-                    </Link>
-                    
-                </div>
-
-
-            </header>
-        </>
-    );
-}
- 
-
+        <div className="header__content">
+          <input
+            className="header__search-input"
+            type="text"
+            id="search"
+            name="search"
+            placeholder="Search"
+          />
+          <img src={avatar} className="header__avatar" />
+          <Link className="header__upload" to="/upload">
+            UPLOAD
+          </Link>
+        </div>
+      </header>
+    </>
+  );
+};
 
 export default Header;
--- src/components/VideoList/VideoList.tsx
+++ src/components/VideoList/VideoList.tsx
@@ -1,30 +1,24 @@
-import Video from "../Video/Video"
-import { VideosDataType } from '../../modules/types';
-import '../VideoList/VideoList.scss';
+import Video from "../Video/Video";
+import { VideosDataType } from "../../modules/types";
+import "../VideoList/VideoList.scss";
 
 interface VideoListProp {
-    videos: VideosDataType[]
+  videos: VideosDataType[];
 }
 
-const VideoList = ({videos}: VideoListProp) => {
+const VideoList = ({ videos }: VideoListProp) => {
+  return (
+    <>
+      <aside className="videos">
+        <h2 className="videos__title"> Next Videos </h2>
+        <ul className="videos__list">
+          {videos.map((video) => (
+            <Video key={video.id} video={video} />
+          ))}
+        </ul>
+      </aside>
+    </>
+  );
+};
 
-    return (  
-        <>
-            <aside className='videos'>
-                <h2 className="videos__title"> Next Videos </h2>
-                <ul className='videos__list'>
-                    {videos.map((video) => 
-                        <Video  
-                                key={video.id} 
-                                video = {video}
-                        />
-                    )}
-                </ul>
-
-            </aside>
-        </>
-
-    );
-}
- 
 export default VideoList;
